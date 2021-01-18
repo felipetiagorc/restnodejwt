0abf4ff5f80a2b51735a56bbd353ba91e797f0c3
Felipe <felipetiagorc@yahoo.com.br>
2021-01-18 02:55:06 -0300

    helmet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var helmet = require('helmet');
 
 const app = express();
+app.use(helmet());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
